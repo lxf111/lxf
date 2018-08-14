2dcc904fe790c4b4e2aeb40ddec00566bc084585
zhanghangfei <1054334426@qq.com>
2018-08-14 15:15:24 +0800

    first

--- app/src/main/java/com/lxk/lxf/ui/home/activity/WiatomActivity.java
+++ app/src/main/java/com/lxk/lxf/ui/home/activity/WiatomActivity.java
@@ -2,6 +2,7 @@ package com.lxk.lxf.ui.home.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,7 +15,7 @@ import com.lxk.lxf.view.MyGridView;
  * Created by Administrator on 2018/8/13 0013.
  */
 
-public class WiatomActivity extends BaseActivity{
+public class WiatomActivity extends BaseActivity implements View.OnClickListener {
     private ImageView ivAdver;
     private TextView tvWiatom;
     private TextView tvLift;
@@ -48,17 +49,21 @@ public class WiatomActivity extends BaseActivity{
         tvChange = (TextView) findViewById(R.id.tv_change);
         tvSignMore = (TextView) findViewById(R.id.tv_sign_more);
         gvSign = (MyGridView) findViewById(R.id.gv_sign);
+        gvSign.setFocusable(false);
         tvTrainMore = (TextView) findViewById(R.id.tv_train_more);
         gvTrain = (MyGridView) findViewById(R.id.gv_train);
+        gvTrain.setFocusable(false);
         tvInterestMore = (TextView) findViewById(R.id.tv_interest_more);
         gvInterest = (MyGridView) findViewById(R.id.gv_interest);
+        gvInterest.setFocusable(false);
         tvStoryMore = (TextView) findViewById(R.id.tv_story_more);
         gvStory = (MyGridView) findViewById(R.id.gv_story);
+        gvStory.setFocusable(false);
     }
 
     @Override
     protected void initData() {
-        recommendAdapter=new HomeRecommendAdapter(context);
+        recommendAdapter = new HomeRecommendAdapter(context);
         gvSign.setAdapter(recommendAdapter);
         gvTrain.setAdapter(recommendAdapter);
         gvInterest.setAdapter(recommendAdapter);
@@ -67,6 +72,27 @@ public class WiatomActivity extends BaseActivity{
 
     @Override
     protected void initEvent() {
+        tvSignMore.setOnClickListener(this);
+        tvTrainMore.setOnClickListener(this);
+        tvInterestMore.setOnClickListener(this);
+        tvStoryMore.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //考试报名
+            case R.id.tv_sign_more:
+                break;
+            //学习培训
+            case R.id.tv_train_more:
+                break;
+            //兴趣爱好
+            case R.id.tv_interest_more:
+                break;
+            //故事
+            case R.id.tv_story_more:
+                break;
+        }
     }
 }
